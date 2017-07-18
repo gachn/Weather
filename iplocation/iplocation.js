0c135f04eeb5bfb8c7638263719a4f16985839fa
Gaurav Chauhan <chauhangaurav101@gmail.com>
2017-07-18 20:31:21 +0530

    console ip

--- iplocation/iplocation.js
+++ iplocation/iplocation.js
@@ -1,14 +1,16 @@
 const request=require('request');
 
 var findLocation=(ip,callback)=>{
-	var ipUrl=`http://ip-api.com/json/+${ip}`;
-	request({url:ip,json:true},(error,response,body)=>{
-	if(!error && response.statusCode === 200){
+	var ipUrl=`http://ip-api.com/json/${ip}`;
+	console.log(`${ipUrl}`);
+	request({url:ipUrl,json:true},(error,response,body)=>{
+	(!error && response.statusCode === 200){
 		var result={
 			latitude: body.lat,
-			longitude: body.lng, 
+			longitude: body.lon, 
 			address: `${body.city} , ${body.country}`
 		}
+
 		callback(undefined,result);
 	}
 	else{
